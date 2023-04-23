a8740ccd8071c50bf33727eed9e2a0f353fcc391
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-23 19:48:54 -0300

    Create main.go

---
+++ array/main.go
@@ -0,0 +1,65 @@
+// "Strange" array manipulation
+
+package main
+
+import "fmt"
+
+func main() {
+  a := [...]int{0, 1, 2, 3}
+	x := a[:1]
+	y := a[2:]
+	x = append(x, y...)
+	x = append(x, y...)
+	fmt.Println(a, x)
+  
+  // output: [0 2 3 3] [0 2 3 3 3]
+  // Editing a slice will edit the content of the original array
+  
+	var y = []string{"A", "B", "C", "D"}
+	var x = y[:3]
+	fmt.Printf("%v", x)
+	for i, s := range x {
+		print(i, s, ",")
+		x = append(x, "Z")
+		x[i+1] = "Z"
+	}
+  // output: 0A,1Z,2C,
+  
+  var x = []string{"A", "B", "C"}
+
+	for i, s := range x {
+		print(i, s, " ")
+		x = append(x, "Z")
+		x[i+1] = "Z"
+	}
+  // output: 0A,1B,2C,
+  
+  var x = []string{"A", "B", "C"}
+
+	for i, s := range x {
+		print(i, s, ",")
+		x[i+1] = "M"
+		x = append(x, "Z")
+		x[i+1] = "Z"
+	}
+  
+  // output: 0A,1M,2C,
+  
+  
+  /*
+  Key points:
+
+    Ranging over a container iterates the elements of a copy of the container. The evaluation of the copy happens before executing the loop, 
+    so the length and capacity of the copy are never changed.
+
+    A slice references its elements on a backing array. So a copy of a slice shares the same elements (and the backing array) with the slice.
+
+    The assignment x[i+1] = "M" in the first loop step modifies the second element of the initial slice x and the copy of the initial slice x.
+    If the free element slots of the first argument slice of an append call are insufficient to hold all the appended elements, 
+    the append call will create a new backing array to hold all the elements of the first argument slice and the appended elements. 
+    So, at the end of the first loop step, the backing array of the slice x is changed. 
+    However, the change doesn't affect the slice copy used in the element iteration. 
+    All subsequent element modifications apply to the new backing array, so they have no effects on the copy used in the element iteration.
+  
+  */
+}
