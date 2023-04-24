21dc137e81dbac4ff28931d2de40e4c90a210abb
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-24 17:53:10 -0300

    Create rectangle.go

---
+++ basics/structs/rectangle.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+var pl = fmt.Println
+
+type rectangle struct {
+	length, height float64
+}
+
+func (r rectangle) Area() float64 {
+	return r.length * r.height
+}
+
+func main() {
+	rect1 := rectangle{10.0, 15.0}
+	pl("Rect Area:", rect1.Area())
+}
