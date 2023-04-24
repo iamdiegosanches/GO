e617522c92dec961e6813ae286dd390ae4b9b9e3
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-24 18:47:02 -0300

    Create mypackage.go

---
+++ basics/accessors-and-mutators/mypackage/mypackage.go
@@ -0,0 +1,48 @@
+package stuff
+
+import (
+	"errors"
+	"time"
+)
+
+type Date struct {
+	day int
+	month int 
+	year int 
+}
+
+func (d *Date) SetDay(day int) error {
+	if (day < 1) || (day > 31) {
+		return errors.New("incorrect day value")
+	}
+	d.day = day
+	return nil
+}
+
+func (d *Date) SetMonth(month int) error {
+	if (month < 1) || (month > 12) {
+		return errors.New("incorrect month value")
+	}
+	d.month = month
+	return nil
+}
+
+func (d *Date) SetYear(year int) error {
+	if (year < 1875) || (year > time.Now().Year()) {
+		return errors.New("incorrect day value")
+	}
+	d.year = year
+	return nil
+}
+
+func (d *Date) Day() int {
+	return d.day
+}
+
+func (d *Date) Month() int {
+	return d.month
+}
+
+func (d *Date) Year() int {
+	return d.year
+}
