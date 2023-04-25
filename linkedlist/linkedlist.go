8f1d513605e97784e60b04357bbb61be61c1f446
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-25 15:31:27 -0300

    Update linkedlist.go

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Node struct {
 	next *Node
@@ -11,6 +13,10 @@ type List struct {
 	head *Node
 	tail *Node
 	size int
+}
+
+func (L *List) GetSize() int {
+	return L.size
 }
 
 func (L *List) Insert(value interface{}) {
@@ -30,10 +36,32 @@ func (L *List) Insert(value interface{}) {
 
 func (L *List) Display() {
 	list := L.head
-	for list != nil {
+	for i := 0; i < L.GetSize(); i++ {
 		fmt.Printf("%+v ->", list.value)
 		list = list.next
 	}
+}
+
+func (L *List) ReverseList() {
+	current := L.head
+	previous, next := &Node{}, &Node{}
+	for current != nil {
+		next = current.next
+		current.next = previous
+		previous = current
+		current = next
+	}
+	L.head = previous
+}
+
+// TODO: implement search function
+func (L *List) Search(val interface{})  {
+	panic("Unimplemented function")
+}
+
+// TODO: implement delete function
+func (L *List) Delete(val interface{}) {
+	panic("Unimplemented function")
 }
 
 func main() {
@@ -41,6 +69,11 @@ func main() {
 	myList.Insert(1)
 	myList.Insert(2)
 	myList.Insert(3)
+	myList.Insert(4)
+	myList.Insert(5)
+
+	myList.ReverseList()
 
 	myList.Display()
+
 }
