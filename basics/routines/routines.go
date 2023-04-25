86db52158dad0200d706dec620d31a923d2f5816
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-25 10:35:58 -0300

    Create routines.go

---
+++ basics/routines/routines.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var pl = fmt.Println
+
+func printTo15() {
+	for i := 1; i <= 15; i++ {
+		pl("Fun 1:", i)
+	}
+}
+
+func printTo10() {
+	for i := 1; i <= 10; i++ {
+		pl("Fun 2:", i)
+	}
+}
+
+func main() {
+	go printTo15()
+	go printTo10()
+	time.Sleep(2 * time.Second)
+}
