72eb10fad296f91423767c6858f3d9d92ab2a0fb
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-27 09:41:32 -0300

    Create singlylinkedlist_test.go

---
+++ linkedlist/singlylinkedlist_test.go
@@ -0,0 +1,27 @@
+package linkedlist
+import("testing")
+
+func TestLinkedList(t *testing.T) {
+	list := &List{}
+	list.Insert(1)
+	list.Insert(2)
+	list.Insert(3)
+
+	if list.GetSize() != 3 {
+		t.Errorf("Expected size to be 3, got %d", list.GetSize())
+	}
+
+	if list.Search(2) != 2 {
+		t.Errorf("Expected to find value 2 in the list")
+	}
+
+	if list.Search(4) != nil {
+		t.Errorf("Expected to not find value 4 in the list")
+	}
+
+	list.ReverseList()
+
+	if list.head.value != 3 {
+		t.Errorf("Expected head value to be 3 after reversing the list")
+	}
+}
