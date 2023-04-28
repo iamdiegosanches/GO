9b8fff4125bb6cce31aa569c0fb976c9892757e7
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-28 12:50:05 -0300

    Update stack_test.go

--- dataStructures/stack/stack_test.go
+++ dataStructures/stack/stack_test.go
@@ -1,1 +1,27 @@
 package stack
+import("testing")
+
+func TestStack(t *testing.T) {
+	myStack := New()
+	if myStack.Len() != 0 {
+		t.Errorf("Expected length of new stack to be 0, got %d", myStack.Len())
+	}
+
+	myStack.Push(1)
+	if myStack.Len() != 1 {
+		t.Errorf("Expected length of stack after push to be 1, got %d", myStack.Len())
+	}
+
+	if myStack.Peek() != 1 {
+		t.Errorf("Expected top value of stack to be 1, got %v", myStack.Peek())
+	}
+
+	val := myStack.Pop()
+	if val != 1 {
+		t.Errorf("Expected popped value to be 1, got %v", val)
+	}
+
+	if myStack.Len() != 0 {
+		t.Errorf("Expected length of stack after pop to be 0, got %d", myStack.Len())
+	}
+}
