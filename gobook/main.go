e55e5611db8cb84d345bd89819d65f7652910aa1
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-30 13:00:41 -0300

    Update main.go

--- gobook/main.go
+++ gobook/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,12 +8,8 @@ func main() {
 	router := gin.Default()
 
 	s := NewAPIServer(":8080")
-	router.GET("/books", func(c *gin.Context) {
-		err := s.handleGetBook(c)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-	})
+	router.GET("/books", makeHTTPHandleFunc(s.handleGetBook))
+	router.POST("/books", makeHTTPHandleFunc(s.handleCreateBook))
 
 	router.Run()
 }
