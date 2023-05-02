3ddf4f6bc6dea152a66c413f10e527daa3f1800b
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-05-02 19:35:50 -0300

    Create main_test.go

---
+++ gobook/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func SetUpRuter() *gin.Engine {
+	router := gin.Default()
+	return router
+}
+
+func TestPostBookHandler(t *testing.T) {
+	router := SetUpRuter()
+	router.POST("/books", handlePostBooks)
+	book := NewBook("Test", "Test Author", time.Now(), "Test Publisher")
+	jsonValue, _ := json.Marshal(book)
+	req, _ := http.NewRequest("POST", "/books", bytes.NewBuffer(jsonValue))
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusCreated, w.Code)
+}
+
+func TestGetBooksHandler(t *testing.T) {
+
+}
+
+func TestDeleteBooksHandler(t *testing.T) {
+
+}
