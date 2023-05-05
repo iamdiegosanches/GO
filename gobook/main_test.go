9707dcb26ff6b345b5ea88a9da16b83ca7a1c589
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-05-04 21:29:18 -0300

    Update main_test.go

--- gobook/main_test.go
+++ gobook/main_test.go
@@ -41,5 +41,27 @@ func TestGetBooksHandler(t *testing.T) {
 }
 
 func TestDeleteBooksHandler(t *testing.T) {
+	router := SetUpRuter()
+	router.POST("/books", handlePostBooks)
+	router.DELETE("/books/:id", handleDeleteBook)
 
+	book := NewBook("Test", "Test Author", time.Now(), "Test Publisher")
+	jsonValue, _ := json.Marshal(book)
+
+	// Create a new book
+	reqCreate, _ := http.NewRequest("POST", "/books", bytes.NewBuffer(jsonValue))
+	wCreate := httptest.NewRecorder()
+	router.ServeHTTP(wCreate, reqCreate)
+	assert.Equal(t, http.StatusCreated, wCreate.Code)
+
+	// Unmarshal the response body to get the ID of the created book
+	var createdBook Book
+	json.Unmarshal(wCreate.Body.Bytes(), &createdBook)
+
+	// Delete the created book
+	reqDel, _ := http.NewRequest("DELETE", "/books/"+createdBook.ID.String(), bytes.NewBuffer(jsonValue))
+	wDel := httptest.NewRecorder()
+	router.ServeHTTP(wDel, reqDel)
+
+	assert.Equal(t, http.StatusOK, wDel.Code)
 }
